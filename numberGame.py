6fbb4136d611be7e11187c7769c41bd4096e782e
Nurha <nurha@projectcodex.co>
2015-09-25 15:42:59 +0200

    in progress

--- numberGame.py
+++ numberGame.py
@@ -1,5 +1,9 @@
 print'Wellcome to Guess The Number game'
 print 'What is your name?'
 yourName = input()
+print 'Can you guess the right number between 1 and 100?'
 
 import random
+number = random.randint(1,100)
+yourGuess = input()
+yourGuess = int(yourGuess) # check if answer is an interger
